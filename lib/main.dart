bcc3024abdc9792972151d19c7c9414a7173ede8
Anmol Gupta <anmolgupta02@gmail.com>
2020-07-11 03:09:03 +0530

    Implemented FlatButtons and OnPressed Methods
    Signed-off-by: anmol <anmolgupta02@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.blue.shade400,
-          appBar: AppBar(
-            title: Text('Dicee'),
-            backgroundColor: Colors.blue.shade500,
-          ),
+        backgroundColor: Colors.blue.shade400,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.blue.shade500,
+        ),
         body: DicePage(),
       ),
     ),
@@ -18,7 +19,33 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+            onPressed: () {
+              print('Left Button Got Clicked');
+            },
+            child: Image.asset('images/dice2.png'),
+          )
+//          Image(
+//            image: AssetImage('images/dice1.png'),
+//          ),
+              ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right Button Got Clicked');
+              },
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
-
 }
